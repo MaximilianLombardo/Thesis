4e27f8b19bc9c5203e12b85056e7b856cc2ff938
Maximilian Lombardo <max@Maximilians-MacBook-Air.local>
2018-10-19 10:43:59 -0400

    String kernels, nearest neighbor instead of numeric features for computing similarity

--- utils/runDREAMPipeline.R
+++ utils/runDREAMPipeline.R
@@ -18,10 +18,10 @@ runDreamPipeline <- function(object, bmtmkl.dir = "~/Documents/gitRepos/master/u
   
   ##############################################################
   #Normalization is already done, but we will need to construct the kernel matrices and set as arrays
+  k <- 5
+  kernels <- lapply(object, FUN = function(data.view){getKernel(data.view, k)})
   
-  kernels <- lapply(object, FUN = RBFKernel)
-  
-  ar <- array(kernels, c(nrow(kernels[[1]]), ncol(kernels[[1]]), length(kernels)));
+  #ar <- array(kernels, c(nrow(kernels[[1]]), ncol(kernels[[1]]), length(kernels)));
   kernel.array <- do.call(abind, c(kernels, list(along = 3)))
   
   
@@ -92,17 +92,25 @@ runDreamPipeline <- function(object, bmtmkl.dir = "~/Documents/gitRepos/master/u
 
 
 
-RBFKernel <- function(data.view){
+getKernel <- function(data.view, k = 5){
   require(kernlab)
   require(parallel)
   require(FNN)
   
+  #Calculate nearest neighbors
+  neighbors <- FNN::get.knn(data = t(data.view), k = k)
+  neighbor.strings <- apply(neighbors$nn.index,1,paste,collapse=" ")
   #Choose sigma 
-  sigma <- sqrt(nrow(data.view))
+  #sigma <- sigma
   #make kernel
-  kernel <- rbfdot(sigma = sigma)
+  #kernel <- rbfdot(sigma = sigma)
+  #kernel <- laplacedot()
+  kernel  <- stringdot(type="string", length = 3)
   #transform data view into features
-  features <- kernelMatrix(kernel = kernel, x = t(data.view))
+  #features <- kernelMatrix(kernel = kernel, x = t(data.view))
+  #features <- kernelMatrix(kernel = kernel, x = neighbors$nn.index)
+  features <- kernelMatrix(kernel = kernel, x = neighbor.strings)
+  
   
   return(features)
   
@@ -110,5 +118,75 @@ RBFKernel <- function(data.view){
 
 
 
+runMKKM <- function(kernel.array, clusters = 2, iter = 10){
+  #source(fxn.location)
+  require(MKKC)
+  
+  model <- list()
+  
+  #mehmet
+  parameters <- list()
+  parameters$cluster_count <- clusters
+  parameters$iteration_count <- iter
+  model$state <- mkkmeans_train(kernel.array, parameters)
+  model$combined.kernel <- combineKernels(kernel.array, model$state$theta)
+  
+  return(model)
+  
+}
 
+##############################################################################################################
 
+mkkmeans_train <- function(Km, parameters) {
+  require(Rmosek)
+  state <- list()
+  state$time <- system.time({
+    P <- dim(Km)[3]
+    theta <- rep(1 / P, P)
+    K_theta <- matrix(0, nrow(Km), ncol(Km))
+    for (m in 1:P) {
+      K_theta <- K_theta + theta[m]^2 * Km[,,m]  
+    }
+    
+    objective <- rep(0, parameters$iteration_count)
+    for (iter in 1:parameters$iteration_count) {
+      #print(sprintf("running iteration %d...", iter))
+      H <- eigen(K_theta, symmetric = TRUE)$vectors[, 1:parameters$cluster_count]
+      
+      problem <- list()
+      problem$sense <- "min"
+      problem$c <- rep(0, P)
+      problem$A <- Matrix(1, nrow = 1, ncol = P, sparse = TRUE)
+      problem$bc <- rbind(blc = 1, buc = 1) 
+      problem$bx <- rbind(blx = rep(0, P), bux = rep(1, P))
+      problem$qobj <- list(i = 1:P, j = 1:P, v = sapply(1:P, function(m) {sum(diag(Km[,,m])) - sum(diag(t(H) %*% Km[,,m] %*% H))}))
+      opts <- list()
+      opts$verbose <- 0
+      result <- mosek(problem, opts)
+      theta <- result$sol$itr$xx
+      K_theta <- matrix(0, nrow(Km), ncol(Km))
+      for (m in 1:P) {
+        K_theta <- K_theta + theta[m]^2 * Km[,,m]  
+      }
+      
+      objective[iter] <- sum(diag(t(H) %*% K_theta %*% H)) - sum(diag(K_theta))
+    }
+    H_normalized <- H / matrix(sqrt(rowSums(H^2, 2)), nrow(H), parameters$cluster_count, byrow = FALSE)
+    
+    set.seed(NULL)
+    state$clustering <- kmeans(H_normalized, centers = parameters$cluster_count, iter.max = 1000, nstart = 10)$cluster
+    state$objective <- objective
+    state$parameters <- parameters
+    state$theta <- theta
+  })
+  return(state)
+}
+##################################################################################
+combineKernels <- function(kernels, kernel.params){
+  for(i in 1:length(kernel.params)){
+    #print(i)
+    kernels[, ,i] <- kernels[, ,i] * kernel.params[i]
+  }
+  return(apply(kernels, c(1,2), sum))
+}
+##############################################################################################################
